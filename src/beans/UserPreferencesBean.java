c729b36729caf0b856f999e9712ba4346f994d43
Jonas Bryllert <Jonas.Bryllert@gmail.com>
2013-01-15 13:31:49 +0000

    added hens page

--- src/beans/UserPreferencesBean.java
+++ src/beans/UserPreferencesBean.java
@@ -3,22 +3,24 @@ package beans;
 import java.io.Serializable;
 import java.util.Locale;
 
-import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 
-@Named("userPreferences")
+@ManagedBean(name="userPreferences")
 @SessionScoped
 public class UserPreferencesBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	private String language;
 
 	public UserPreferencesBean() {
 		System.out.println("UserPreferencesBean constructor");
 	}
 	
-	public String setLanguage(String language) {
-		System.out.println("setLanguage - " + language);
+	public String changeLanguage(String language) {
+		System.out.println("changeLanguage - " + language);
+		this.language = language;
 		if ("swedish".equalsIgnoreCase(language)) {
 			FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("sv"));
 		}
@@ -26,6 +28,10 @@ public class UserPreferencesBean implements Serializable {
 			FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ENGLISH);
 		}
 		return null;
+	}
+	
+	public String getLanguage() {
+		return language;
 	}
 	
 }
